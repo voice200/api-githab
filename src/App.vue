83d43d0389be4bff53f1fa617be802afec226dc1
voice200 <voice200rabota@gmail.com>
2021-04-03 20:40:50 +0200

    added prettier, vuex, router and structure project

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div id="app">
+
+    <router-view/>
   </div>
 </template>
 
-<script>
-
-export default {
-  name: 'App',
-  components: {
-  }
-}
-</script>
-
 <style lang="scss">
-#app{
+#app {
   width: 100%;
   height: 100%;
 }
 body{
-  margin: 0 auto;
-}
 
+}
 </style>
